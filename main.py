5dbdb701da70aa4685867c2f13e2cfa5807657f5
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-02-11 12:09:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def most_common(lst):
         return round(sum(common_things) / len(common_things), 1)
 
 
+# finds slopes that are similar to the most common slope and returns those lines
 def find_similar(lines, slopes, most):
     good_lines = []
     rounded = []
@@ -61,9 +62,13 @@ def find_similar(lines, slopes, most):
         except OverflowError:
             rounded.append(999)
 
+    # if the slope is horizontal, then make it so that the slopes have to be within 2 of the most common slope
     for i in range(len(lines)):
         if -10 <= rounded[i] <= 10:
             increment = 2
+        # otherwise, make it so the slopes have to be within 100 of the most common slope
+        # this is because vertical slopes have a higher margin of error and will vary more greatly with small movements
+        # whereas horizontal slopes will have a small variation
         else:
             increment = 100
         if float(most + increment) > rounded[i] > float(most - increment):
@@ -72,13 +77,15 @@ def find_similar(lines, slopes, most):
     return good_lines
 
 
+# detect and output the lines and middle lines of the frame given
 def center(og):
+    # apply the filters and detect the lines with HoughLinesP
     frame = filters(og)
     lines = cv2.HoughLinesP(frame, 1, np.pi / 180, threshold=120, minLineLength=100, maxLineGap=999999)
     cv2.rectangle(og, (500, 100), (1300, 1000), 255, 10)
-    endpts = []
     slopes = []
     avg_x1, avg_y1, avg_x2, avg_y2 = 0, 0, 0, 0
+    # find the slopes of each line detected
     if lines is not None:
         for i in lines:
             x1, y1, x2, y2 = i[0]
@@ -87,28 +94,30 @@ def center(og):
             except ZeroDivisionError:
                 slopes.append(999)
 
+        # find the most common slope in the frame
         freq = most_common(slopes)
+        # find the lines with similar slopes to the most common slope and display them
         good_lines = find_similar(lines, slopes, freq)
         for i in good_lines:
             x1, y1, x2, y2 = i[0]
             cv2.line(og, (x1, y1), (x2, y2), (0, 0, 255), 10)
 
+            # add the coordinates of each line to a total sum
             avg_x1 += x1
             avg_y1 += y1
             avg_x2 += x2
             avg_y2 += y2
 
-            endpts.append(((x1, y1), (x2, y2)))
-
+        # to prevent a divide by zero error, check to make sure at least some lines were outputted
+        # calculate the average of all the endpoints of the outputted lines
         if len(good_lines) > 0:
             avg_x1 /= len(good_lines)
             avg_y1 /= len(good_lines)
             avg_x2 /= len(good_lines)
             avg_y2 /= len(good_lines)
 
-        avg_line = (int(avg_x1), int(avg_y1), int(avg_x2), int(avg_y2))
-        cv2.line(og, (avg_line[0], avg_line[1]), (avg_line[2], avg_line[3]), (0, 255, 0), 10)
-
+        # display the line
+        cv2.line(og, (int(avg_x1), int(avg_y1)), (int(avg_x2), int(avg_y2)), (0, 255, 0), 10)
     else:
         print("no lines")
         return og
@@ -136,4 +145,3 @@ while video.isOpened():
 # stop the video capture object and destroy all the open cv windows
 video.release()
 cv2.destroyAllWindows()
-
