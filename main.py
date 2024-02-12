c9bdbc31d691e13dbe949d485fc6cbb5202f5718
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-02-12 13:08:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import cv2
 import numpy as np
+import math
 
 
-# applies the filters for grayscale, gaussian blur, canny edge, and a rectangle mask onto the frame given
+# applies the filters for grayscale, gaussian blur, thresholding, canny edge,
+# and a rectangle mask onto the frame given
 def filters(pic):
     bw = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
-    gauss_bw = cv2.GaussianBlur(bw, (15, 15), 0)
-    canny_gauss_bw = cv2.Canny(gauss_bw, 100, 200, apertureSize=5)
+    thresh = cv2.threshold(bw, 165, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow("thresh", thresh)
+    canny_gauss_bw = cv2.Canny(thresh, 150, 200, apertureSize=3, L2gradient=True)
+    cv2.imshow("canny", canny_gauss_bw)
     mask = np.zeros(pic.shape[:2], dtype="uint8")
     cv2.rectangle(mask, (500, 100), (1300, 1000), 255, -1)
     masked = cv2.bitwise_and(canny_gauss_bw, canny_gauss_bw, mask=mask)
     return masked
+
+
+# finds slopes that are similar to the most common slope and returns those lines
+def find_similar(lines, slopes, most):
+    good_lines = []
+    rounded = []
+    for x in slopes:
+        try:
+            x = round(x)
+            rounded.append(x)
+        except OverflowError:
+            rounded.append(99)
+
+    # if the slope is horizontal, then make it so that the slopes have to be within 2 of the most common slope
+    for i in range(len(lines)):
+        # if -5 <= rounded[i] <= 5:
+        #     increment = 2
+        # # otherwise, make it so the slopes have to be within 100 of the most common slope
+        # # this is because vertical slopes have a higher margin of error and will vary more greatly with small movements
+        # # whereas horizontal slopes will have a small variation
+        # else:
+        #     increment = 60
+        try:
+            if abs(rounded[i] - rounded[i + 1]) == 0:
+                good_lines.append(lines[i])
+                good_lines.append(lines[i + 1])
+        except:
+            continue
+    return good_lines
 
 
 # given a list, rounds all values and finds the most common value and returns that
@@ -47,51 +80,21 @@ def most_common(lst):
         return round(sum(common_things) / len(common_things), 1)
 
 
-# finds slopes that are similar to the most common slope and returns those lines
-def find_similar(lines, slopes, most):
-    good_lines = []
-    rounded = []
-    for x in slopes:
-        try:
-            x = round(x)
-            rounded.append(x)
-        except OverflowError:
-            rounded.append(99)
-
-    # if the slope is horizontal, then make it so that the slopes have to be within 2 of the most common slope
-    for i in range(len(lines)):
-        if -5 <= rounded[i] <= 5:
-            increment = 2
-        # otherwise, make it so the slopes have to be within 100 of the most common slope
-        # this is because vertical slopes have a higher margin of error and will vary more greatly with small movements
-        # whereas horizontal slopes will have a small variation
-        else:
-            increment = 60
-        if float(most + increment) > rounded[i] > float(most - increment):
-            good_lines.append(lines[i])
-    print("most", most)
-    print("lines", len(lines))
-    print("rounde", rounded)
-    print("good", len(good_lines))
-    return good_lines
-
-
 # detect and output the lines and middle lines of the frame given
 def center(og):
     # apply the filters and detect the lines with HoughLinesP
     frame = filters(og)
-    lines = cv2.HoughLinesP(frame, 1, np.pi / 180, threshold=120, minLineLength=100, maxLineGap=999999999)
+    lines = cv2.HoughLinesP(frame, rho=1, theta=np.pi / 180, threshold=100, minLineLength=50, maxLineGap=999)
     cv2.rectangle(og, (500, 100), (1300, 1000), 255, 10)
     slopes = []
-    mid = 0
+    good = []
     # find the slopes of each line detected
     if lines is not None:
         for i in lines:
             x1, y1, x2, y2 = i[0]
             try:
                 slopes.append(((y2 - y1) / (x2 - x1)))
-            except Exception as e:
-                print(e)
+            except RuntimeWarning or ZeroDivisionError:
                 slopes.append(99)
 
         # find the most common slope in the frame
@@ -99,21 +102,43 @@ def center(og):
         # find the lines with similar slopes to the most common slope and display them
         good_lines = find_similar(lines, slopes, freq)
 
-        coord = []
-        for i in good_lines:
-            x1g, y1g, x2g, y2g = i[0]
-            cv2.line(og, (x1g, y1g), (x2g, y2g), (0, 0, 255), 10)
-            coord.append([[x1g, y1g], [x2g, y2g]])
+        good_dist = []
 
-        # to prevent a divide by zero error, check to make sure at least some lines were outputted
-        # calculate the average of two of the endpoints of the outputted lines
-        if len(coord) >= 2:
-            ax = int((coord[0][0][0] + coord[1][0][0]) / 2)
-            ay = int((coord[0][0][1] + coord[1][0][1]) / 2)
-            bx = int((coord[0][1][0] + coord[1][1][0]) / 2)
-            by = int((coord[0][1][1] + coord[1][1][1]) / 2)
-            # display the line
-            cv2.line(og, (ax, ay), (bx, by), (0, 255, 0), 10)
+        if good_lines is not None:
+
+            # loop through the good lines from the slopes
+            for i in good_lines:
+                x1, y1, x2, y2 = i[0]
+                print("lines", len(good_lines))
+
+                # Initialize a variable to check if the lines are too close
+                # find the distance between the new line points and the old line
+                dist = any(
+                    math.dist([x1, y1], [pt1[0], pt1[1]]) < 100 or
+                    math.dist([x2, y2], [pt2[0], pt2[1]]) < 100
+                    for pt1, pt2 in good_dist
+                )
+
+                # If the lines are farther than 100px apart, append the new lines to the new list
+                # and draw the line on the image
+                if not dist:
+                    good_dist.append([[x1, y1], [x2, y2]])
+                    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+            # to prevent a divide by zero error, check to make sure that at least 1 line was detected
+            # calculate the average of two of the endpoints of the outputted lines
+            if good_dist is not None:
+                print("dist", len(good_dist))
+                for i in range(len(good_dist)):
+                    cv2.line(og, (good_dist[i][0][0], good_dist[i][0][1]),
+                             (good_dist[i][1][0], good_dist[i][1][1]), (0, 0, 255), 10, cv2.LINE_AA)
+                    good.append(
+                        (good_dist[i][0][0], good_dist[i][0][1], good_dist[i][1][0], good_dist[i][1][1]))
+                if len(good) >= 2:
+                    cv2.line(og,
+                             (round((good[0][0] + good[1][0]) / 2), round((good[0][1] + good[1][1]) / 2)),
+                             (round((good[0][2] + good[1][2]) / 2), round((good[0][3] + good[1][3]) / 2)),
+                             (0, 255, 0), 10, cv2.LINE_AA)
     else:
         return og
     return og
@@ -132,7 +157,7 @@ while video.isOpened():
 
         # show the frame
         cv2.imshow("Detected Frame", center_frame)
-        if cv2.waitKey(36) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
